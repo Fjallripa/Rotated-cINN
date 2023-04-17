f46f012e76531ad8a06a28d9135187960ef09056
Fjallripa - Framework <58792390+Fjallripa@users.noreply.github.com>
2023-04-17 16:22:22 +0200

    Begun creating the data preparation
    * defined the class RotatedMNIST which handles loading and adapting the MNIST dataset.
      * contains empty methods as of yet.

--- rotated_cinn/modules/data.py
+++ rotated_cinn/modules/data.py
@@ -1,3 +1,37 @@
 # Dataset preparation
 # ================
 # defines the MnistRotated dataset class
+
+
+# Imports
+import torch.utils.data
+
+
+# Main class
+class RotatedMNIST(torch.utils.data.Dataset):
+    '''
+    Creates a custom RotatedMNIST dataset.
+    '''
+
+    def __init__(self, domains: list[int], train: bool, val_set_size=0) -> None:
+        super().__init__()
+    
+
+    def load_training(self):
+        pass
+
+
+    def load_test(self):
+        pass
+
+
+    def process_data(self):
+        pass
+
+
+    def __len__(self):
+        pass
+
+
+    def __getitem__(self, index):
+        pass
